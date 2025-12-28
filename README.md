528f483360b8704b17f611e16bd9113351169889
tombyrn <thomasbyrnes02@gmail.com>
2025-12-27 23:33:40 -0500

    added options and tweaked data moshing process

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # ffmosh
-Bash script that uses FFMPEG to create datamoshed visuals
+Bash script that uses FFMPEG to create datamoshed .mp4 files
 
 ## Usage 
 
 Simply run the bash script and provide the video file as the second command line argument
 
 `
-./ffmosh.sh file_name.mp4
+./ffmosh.sh <input_file> [-o <output_file> -r <interval>]
 `
+
+`input_file` should be in mp4 format
+
+`output_file` is optional and ".mp4" will be appended if it's not present in the string
+
+`interval` is every i'th frame that will be removed when the i frames are being manipulated in the datamoshing process default is 5
--- ffmosh.sh
+++ ffmosh.sh
@@ -1,48 +1,87 @@
 #! /bin/bash
-# Adapted for UNIX based on reddit post by u/justhadto https://www.reddit.com/r/datamoshing/comments/t46x3i/datamoshing_with_ffmpeg_howto_in_comments/
-# Usage: ffmosh.sh <filename.mp4>
+# Adapted for UNIX script based on reddit post by u/justhadto https://www.reddit.com/r/datamoshing/comments/t46x3i/datamoshing_with_ffmpeg_howto_in_comments/
+# Usage: ./ffmosh.sh <input_file> [-o <output_file> -r <interval>]
 
+filename="$1"
+base="${filename%.mp4}"
+output_file=$base.datamoshed.mp4 # Default output name if none provided
+input_file=""
+interval=5 # Every i'th frame that will be removed
 
 # Check if ffmpeg is installed
 if ! command -v ffmpeg &> /dev/null
 then
-    echo "ffmpeg could not be found"
+    echo "ERROR: ffmpeg could not be found"
     exit 1
 fi
 
 # Check if at least two arguments are provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <filename.mp4>"
+if [ "$#" -lt 1 ] || [ "$#" -gt 6 ]; then
+    echo "Usage: $0 <input_file> [-o <output_file> -r <interval>]"
     exit 1
 fi
 
+# Go through each argument
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -o)
+      output_file="$2"
+      shift 2
+      ;;
+    -o*)
+      output_file="${1#-o}"
+      shift
+      ;;
+    -r)
+      interval="$2"
+      shift 2
+      ;;
+    -r*)
+      interval="${1#-r}"
+      shift
+      ;;
+    -*)
+      echo "Unknown option: $1" >&2
+      exit 1
+      ;;
+    *)
+      input_file="$1"
+      shift
+      ;;
+  esac
+done
+
 # Check if the file exists
-if [ ! -f "$1" ]; then
+if [ ! -f $input_file ]; then
     echo "File not found!"
     exit 1
 fi
 
-file=$1
-RR=5 # Remove Rate: every RRth frame  will be removed
-
-echo "File: $file"
+# Append ".mp4" to output file name if not present
+if [[ $output_file != *.mp4 ]]; then
+    ouput_file="${output_file}.mp4"
+fi
 
 # Convert it to libxvid so that the I-frames will be generated:
-ffmpeg -i $file -vcodec libxvid -q:v 1 -g 1000 -qmin 1 -qmax 1 -flags qpel+mv4 -an -y xvid_video.avi
-
-
-mkdir ./frames  
+ffmpeg -i $input_file -vcodec libxvid -q:v 1 -g 1000 -qmin 1 -qmax 1 -flags qpel+mv4 -an -y xvid_video.avi
 
 # Extract the raw frames
+mkdir ./frames  
 ffmpeg -i xvid_video.avi -vcodec copy -start_number 0 frames/\f_%04d.raw
 
 cd frames
 # Remove every other frame in frames
+prev=""
+i=0
 for f in $(ls -1); do
-    if [ $((i++ % $RR)) -eq $(($RR-1)) ]; then
-        echo "Removing $f"
-        rm $f
+    if [ $i -eq 0 ]; then
+      continue 
     fi
+    if [ $((i++ % $interval)) -eq $(($interval-1)) ]; then
+        cp "$prev" "$f"
+    fi
+    prev=$f
+    ((i++))
 done
 
 # Copy all frames to new file
@@ -52,11 +91,11 @@ for i in $(ls *.raw | sort); do
 done
 
 # Convert the datamoshed video back to mp4
-ffmpeg -i edited_video.avi -i ../$file -vf scale=1280:-2 -map 0:v:0 -map 1:a:0 -vcodec h264 ../final_video.mp4
+ffmpeg -i edited_video.avi -i ../$input_file -vf scale=1280:-2 -map 0:v:0 -map 1:a:0 -vcodec h264 ../$output_file
 
+# Cleanup
 cd ..
 rm -rf frames images xvid_video.avi
-
 
 # Exit the script
 exit 0
